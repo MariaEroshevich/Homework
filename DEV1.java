ade233978be216bcad9da302688a468729534a8a
MariaEroshevich <m.eroshevich97@gmail.com>
2017-06-21 16:36:02 +0400

    Update DEV1.java

--- DEV1.java
+++ DEV1.java
@@ -1,13 +1,11 @@
 public class DEV1 {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
-            if((i % 3) == 0) { 
-                int n = i/3;
-                System.out.println(n + "*3");
-            } else { 
-                System.out.println(i);
-            }
-        }
+  public static void main(String[] args) {
+    for (int i = 0; i <= 100; i++) {
+      if ((i % 3) == 0) { 
+        System.out.println("3*" + i/3);
+      } else { 
+        System.out.println(i);
+      }
     }
+  }
 }
-
