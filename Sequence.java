d0725c849ebeba332491808b80c7213f7e0dda77
MariaEroshevich <m.eroshevich97@gmail.com>
2017-06-28 16:37:30 +0400

    Modify Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -2,21 +2,36 @@ import java.util.Scanner;
 
 public class Sequence {
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    System.out.print("Enter the sequence of numbers: ");
-    String sequence = in.nextLine();
-    String[] sequenceParts = sequence.split(" ");
-    try {
-      for (int i = 1; i < sequenceParts.length; i++) {
-        if (Integer.parseInt(sequenceParts[i]) < Integer.parseInt(sequenceParts[i - 1])) {
-          System.out.println("The entered sequence of numbers is not non-decreasing.");
-          return;
+    if (args.length != 0) {
+      try {
+        for (int i = 1; i < args.length; i++) {
+          if (Integer.parseInt(args[i]) < Integer.parseInt(args[i - 1])) {
+            System.out.println("The entered sequence of numbers is not non-decreasing.");
+            return;
+          }
         }
+      } catch (NumberFormatException e) {
+        System.err.println("You entered an invalid number format.");
+        return;
       }
-    } catch (NumberFormatException e) {
-      System.err.println("You entered an invalid number format.");
-      return;
+      System.out.println("The entered sequence of numbers does not decreasing.");
+    } else {
+      Scanner in = new Scanner(System.in);
+      System.out.print("Enter the sequence of numbers: ");
+      String sequence = in.nextLine();
+      String[] sequenceParts = sequence.split(" ");
+      try {
+        for (int i = 1; i < sequenceParts.length; i++) {
+          if (Integer.parseInt(sequenceParts[i]) < Integer.parseInt(sequenceParts[i - 1])) {
+            System.out.println("The entered sequence of numbers is not non-decreasing.");
+            return;
+          }
+        }
+      } catch (NumberFormatException e) {
+        System.err.println("You entered an invalid number format.");
+        return;
+      }
+      System.out.println("The entered sequence of numbers does not decreasing.");
     }
-    System.out.println("The entered sequence of numbers does not decreasing.");
   }
 }
