f9e1f5fea445ce592c3287362609f84b1ddb6ecc
MariaEroshevich <m.eroshevich97@gmail.com>
2017-07-07 15:53:15 +0300

    Removed an auxiliary variable to call the method output in the Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -3,17 +3,13 @@ public class Sequence {
     try {
       SequenceAnalysis sequenceAnalysis = new SequenceAnalysis();
       if (args.length != 0) {
-        boolean testSequence = sequenceAnalysis.sequenceCheck(args);
-        sequenceAnalysis.messages(testSequence);
+        sequenceAnalysis.output(sequenceAnalysis.sequenceCheck(args));
       } else {
         ReadFromTheConsole readFromTheConsole = new ReadFromTheConsole();
-        String[] textWithConsole = readFromTheConsole.read();
-        boolean testSequence = sequenceAnalysis.sequenceCheck(textWithConsole);
-        sequenceAnalysis.messages(testSequence);
+        sequenceAnalysis.output(sequenceAnalysis.sequenceCheck(readFromTheConsole.read()));
       }
     } catch (NumberFormatException e) {
       System.out.println("You entered an invalid number format.");
     }
   }
 }
-   
