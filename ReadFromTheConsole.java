b15a0a14114b7acf0bfa057651c607077f52a834
MariaEroshevich <m.eroshevich97@gmail.com>
2017-07-08 19:46:27 +0400

    Changed return and comment on the method in the ReadFromTheConsole.java

--- ReadFromTheConsole.java
+++ ReadFromTheConsole.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
  */
 public class ReadFromTheConsole {
   /**
-   * Asks to enter a sequence and displays it on the screen.
-   * @return sequenceParts is an input sequence.
+   * Reads data from the console.
+   * @return input sequence.
    */
   public String[] read() {
     Scanner in = new Scanner(System.in);
     System.out.print("Enter the sequence of numbers: ");
     String sequence = in.nextLine();
     String[] sequenceParts = sequence.split(" ");
-    return sequenceParts;
+    return sequence.split(" ");
   }
 }
