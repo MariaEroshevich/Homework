f75cf92b45d91bba723701abbc9861f8ac766efa
MariaEroshevich <m.eroshevich97@gmail.com>
2017-07-11 00:43:23 +0400

    Removed an extra variable in the ReadFromTheConsole.java

--- ReadFromTheConsole.java
+++ ReadFromTheConsole.java
@@ -12,7 +12,6 @@ public class ReadFromTheConsole {
     Scanner in = new Scanner(System.in);
     System.out.print("Enter the sequence of numbers: ");
     String sequence = in.nextLine();
-    String[] sequenceParts = sequence.split(" ");
     return sequence.split(" ");
   }
 }
