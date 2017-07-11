17bddb6684a9c8c023d48614122e612d4a8bd8fe
MariaEroshevich <m.eroshevich97@gmail.com>
2017-07-11 23:57:40 +0400

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -4,10 +4,10 @@ public class Sequence {
       SequenceAnalysis sequenceAnalysis = new SequenceAnalysis();
       OutputMessages outputMessages = new OutputMessages();
       if (args.length != 0) {
-        outputMessages.outputMessages(sequenceAnalysis.sequenceCheck(args));
+        outputMessages.outputMessages(sequenceAnalysis.sequenceAnalysis(args));
       } else {
         ReadFromTheConsole readFromTheConsole = new ReadFromTheConsole();
-        outputMessages.outputMessages(sequenceAnalysis.sequenceCheck(readFromTheConsole.read()));
+        outputMessages.outputMessages(sequenceAnalysis.sequenceAnalysis(readFromTheConsole.read()));
       }
     } catch (NumberFormatException e) {
       System.out.println("You entered an invalid number format.");
