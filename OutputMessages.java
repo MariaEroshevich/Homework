474451829de28b21790c179dcce185191df498c8
MariaEroshevich <m.eroshevich97@gmail.com>
2017-07-11 23:40:03 +0400

    Changed the name of the argument in the OutputMessages.java

--- OutputMessages.java
+++ OutputMessages.java
@@ -4,10 +4,10 @@
 public class OutputMessages {
   /**
    * Displays the type of sequence.
-   * @param typeOfSequence flag for output of sequence type.
+   * @param isNotDecreasing flag for output of sequence type.
    */
-  public void outputMessages(boolean typeOfSequence) {
-    if (typeOfSequence) {
+  public void outputMessages(boolean isNotDecreasing) {
+    if (isNotDecreasing) {
       System.out.println("The entered sequence of numbers is not nondecreasing.");
     } else {
       System.out.println("The entered sequence of numbers is does not decreasing.");
