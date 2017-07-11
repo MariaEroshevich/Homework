e77c1342899ef50e8f9c71fe5bdcbd337c509aca
MariaEroshevich <m.eroshevich97@gmail.com>
2017-07-11 23:55:59 +0400

    The name of the method and the name of the variable in the SequenceAnalysis.java

--- SequenceAnalysis.java
+++ SequenceAnalysis.java
@@ -4,12 +4,12 @@
 public class SequenceAnalysis {
   /**
    * Defines the input sequence.
-   * @param sequenceParts the sequence that we chek.
+   * @param sequence the sequence that we analysis.
    * @return true, if the sequence decreases.
    */
-  public boolean sequenceCheck(String[] sequenceParts) {
-    for (int i = 1; i < sequenceParts.length; i++) {
-      if (Integer.parseInt(sequenceParts[i]) <= Integer.parseInt(sequenceParts[i - 1])) {
+  public boolean sequenceAnalysis(String[] sequence) {
+    for (int i = 1; i < sequence.length; i++) {
+      if (Integer.parseInt(sequence[i]) <= Integer.parseInt(sequence[i - 1])) {
         return true;
       }     
     } 
